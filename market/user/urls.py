316997633f91b5e15784255b0272a902356df9c8
赵兴川 <15196668159@163.com>
2018-11-19 21:06:16 +0800

    34

--- market/user/urls.py
+++ market/user/urls.py
@@ -1,10 +1,33 @@
 from django.conf.urls import url
 
-from user.views import register, login, forget, info
+from user.views import register, login, forget, info, address, village, index, gladdress, safty, password, payment, \
+    bound_phone, money, record, intergral, intergral_exchange, intergral_records, red_poket, dated, step, about, \
+    collect, collect_edit, job, application
 
 urlpatterns = [
     url(r'^reg/$', register, name="register"),  # 注册
     url(r'^log/$', login, name="login"),  # 登陆
     url(r'^forget/$', forget, name="forget"),  # 忘记密码
+    url(r'^$', index, name="center"),  # 个人中心首页
     url(r'^info/$', info, name="info"),  # 个人资料展示
+    url(r'^step/$', step, name="step"),  # 设置
+    url(r'^about/$', about, name="about"),  # 关于我们
+    url(r'^gladdress/address/$', address, name="address"),  # 新增地址
+    url(r'^gladdress/address/village/$', village, name="village"),  # 校区选择
+    url(r'^gladdress/$', gladdress, name="gladdress"),  # 管理收货地址
+    url(r'^safty/$', safty, name="safty"),  # 安全管理
+    url(r'^safty/password/$', password, name="password"),  # 安全管理
+    url(r'^safty/payment/$', payment, name="payment"),  # 设置支付密码
+    url(r'^safty/bound_phone/$', bound_phone, name="bound_phone"),  # 绑定手机号
+    url(r'^money/$', money, name="money"),  # 我的钱包
+    url(r'^money/red_poket$', red_poket, name="red_poket"),  # 我的红包
+    url(r'^money/red_poket/dated/$', dated, name="dated"),  # 已过期的红包
+    url(r'^money/record/$', record, name="record"),  # 账户余额
+    url(r'^money/intergral/$', intergral, name="intergral"),  # 积分
+    url(r'^money/intergral/exchange/$', intergral_exchange, name="exchange"),  # 积分兑换
+    url(r'^money/intergral/exchange/intergral_records$', intergral_records, name="intergral_records"),  # 积分兑换记录
+    url(r'^collect/$', collect, name="collect"),  # 我的收藏
+    url(r'^collect/edit$', collect_edit, name="edit"),  # 收藏编辑
+    url(r'^job/$', job, name="job"),  # 找兼职
+    url(r'^job/application/$', application, name="application"),  # 兼职申请记录
 ]
--- market/user/views.py
+++ market/user/views.py
@@ -16,6 +16,111 @@ def forget(request):
     return render(request, 'user/forgetpassword.html')
 
 
+def index(request):
+    # 个人中心首页
+    return render(request, 'user/member.html')
+
+
 def info(request):
     # 个人资料展示
     return render(request, 'user/infor.html')
+
+
+def address(request):
+    # 地址管理
+    return render(request, 'user/address.html')
+
+
+def village(request):
+    # 校区选择
+    return render(request, "user/village.html")
+
+
+def gladdress(request):
+    # 收货地址
+    return render(request, 'user/gladdress.html')
+
+
+def safty(request):
+    # 安全设置
+    return render(request, 'user/saftystep.html')
+
+
+def password(request):
+    # 修改登陆密码
+    return render(request, 'user/password.html')
+
+
+def payment(request):
+    # 设置支付密码
+    return render(request, 'user/payment.html')
+
+
+def bound_phone(request):
+    # 绑定手机号
+    return render(request, 'user/boundphone.html')
+
+
+def money(request):
+    # 我的钱包
+    return render(request, 'user/money.html')
+
+
+def record(request):
+    # 账户余额
+    return render(request, 'user/records.html')
+
+
+def intergral(request):
+    # 积分
+    return render(request, 'user/integral.html')
+
+
+def intergral_exchange(request):
+    # 积分兑换
+    return render(request, 'user/integralexchange.html')
+
+
+def intergral_records(request):
+    # 积分兑换记录
+    return render(request, 'user/integralrecords.html')
+
+
+def red_poket(request):
+    # 我的红包
+    return render(request, 'user/red_poket.html')
+
+
+def dated(request):
+    # 已过期的红包
+    return render(request, 'user/ygq.html')
+
+
+def step(request):
+    # 设置
+    return render(request, 'user/step.html')
+
+
+def about(request):
+    # 关于我们
+    return render(request, 'user/about.html')
+
+
+def collect(request):
+    # 我的收藏
+    return render(request, 'user/collect.html')
+
+
+def collect_edit(request):
+    # 编辑收藏
+    return render(request, 'user/collect-edit.html')
+
+
+def job(request):
+    # 我要兼职
+    return render(request,'user/job.html')
+
+
+def application(request):
+    # 兼职申请记录
+    return render(request, 'user/application.html')
